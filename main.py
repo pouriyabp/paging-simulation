75a8439e664818013a4cbcede33875de48b79e49
pouriyabp <pouriya1379@gmail.com>
2021-07-04 07:49:56 +0430

    calculate and show the status of ram.

--- main.py
+++ main.py
@@ -13,6 +13,20 @@ class Process:
         self.size = size
         self.tempSize = size
         self.priority = 0  # set process priority
+        self.frameSize = -1
+        self.splitProcess = []
+
+    def get_frame_size(self, frame_size):
+        self.frameSize = frame_size
+
+    def split_process(self):
+        count = self.size / self.frameSize
+        if int(count) < count:
+            count = int(count) + 1
+        else:
+            count = int(count)
+        for n in range(count):
+            self.splitProcess.append(self.name + "-chunk" + str(n + 1))
 
     def __repr__(self):
         return f"{self.name}"
@@ -23,6 +37,29 @@ class Process:
         :return: nothing return ---> set priority of each food
         """
         self.priority = self.tempDeadline
+
+
+class Ram:
+    def __init__(self, size_of_memory, frame_size):
+        self.size = size_of_memory
+        self.frameSize = frame_size
+
+        count_of_frames = int(self.size / self.frameSize)
+        self.arr_of_frames = Ram.split_memory(count_of_frames)
+
+    @staticmethod
+    def split_memory(count_of_frames):
+        arr_of_frames = []
+        for n in range(count_of_frames):
+            arr_of_frames.append(None)
+        return arr_of_frames
+
+    def __repr__(self):
+        text = "-----------------------\n"
+        for n in self.arr_of_frames:
+            text += "|" + str(n) + "|\n"
+            text += "-----------------------\n"
+        return text
 
 
 def lcm(x, y):
@@ -61,7 +98,8 @@ def earliest_deadline_first(list_of_process, whole_time):
 
     while i <= whole_time:
         if len(temp_list) == 0:
-            scheduling_process.append(None)
+            x = (None, temp_list)
+            scheduling_process.append(x)
             i += 1
             for process in list_of_process:
                 if i % process.period == 0:
@@ -69,13 +107,15 @@ def earliest_deadline_first(list_of_process, whole_time):
             continue
         if i == 0:
             scheduling_process.append(None)
-            print(None)
+            # print(None)
             i += 1
             continue
         min_process = min(temp_list, key=attrgetter('tempDeadline'))
         min_process.tempTime -= 1
-        print(f"{i - 1} {min_process}")
-        scheduling_process.append(min_process)
+        # print(f"{i - 1} {min_process}")
+
+        x = (min_process, tuple(temp_list))
+        scheduling_process.append(x)
         # remove food if it's done
         if min_process.tempTime == 0:
             min_process.priority = 0
@@ -89,13 +129,99 @@ def earliest_deadline_first(list_of_process, whole_time):
             for process in list_of_process:
                 if i % process.period == 0:
                     temp_list.append(process)
+    # remove first None in list
+    scheduling_process.remove(None)
     return scheduling_process
 
 
-process1 = Process("process1", 1, 5, 6, 10)
-process2 = Process("process2", 2, 4, 5, 6)
-process3 = Process("process3", 4, 8, 10, 15)
+def set_frame_size(list_of_process, ram):
+    for process in list_of_process:
+        process.get_frame_size(ram.frameSize)
+        process.split_process()
+
+
+def calculate_memory_process(list_of_process, result_scheduling, whole_time, ram):
+    for i in range(whole_time):
+        time = f"{i}-{i + 1}"
+        temp = result_scheduling[i]
+        process_in_cpu = temp[0]
+        temp_list = temp[1]
+        print(f"{time}: {process_in_cpu} is in the CPU.")
+        print(f"list of process that are wait: {list(temp_list)}")
+
+        # calculate function are in ram
+        # ==============================================================================================================
+        # # remove process that are finished (not in temp list or waiting list)
+        # for process in list_of_process:
+        #     if process not in temp_list:
+        #         for i in range(len(ram.arr_of_frames)):
+        #             if ram.arr_of_frames[i] in process.splitProcess:
+        #                 ram.arr_of_frames[i] = None
+        # ==============================================================================================================
+        list_of_process_in_the_hard = []
+        # put chunk of process must be in ram
+        if process_in_cpu is not None:
+            # check that chunk are in the ram or not
+            all_chunk_in_ram = True
+            for chunk in process_in_cpu.splitProcess:
+                if chunk in ram.arr_of_frames:
+                    continue
+                else:
+                    all_chunk_in_ram = False
+                    break
+            # if chunk are not in ram then put them in ram
+            if all_chunk_in_ram is False:
+                for chunk in process_in_cpu.splitProcess:
+                    put_chunk = False
+                    for i in range(len(ram.arr_of_frames)):
+                        if ram.arr_of_frames[i] is None:
+                            ram.arr_of_frames[i] = chunk
+                            put_chunk = True
+                            break
+                    if put_chunk is False:
+                        for i in range(len(ram.arr_of_frames)):
+                            if ram.arr_of_frames[i] not in process_in_cpu.splitProcess:
+                                # process that go to hard because no enough space in ram
+                                edit = ram.arr_of_frames[i]
+                                edit = edit.split("-")
+                                list_of_process_in_the_hard.append(edit[0])
+                                # replace that process with chunk
+                                ram.arr_of_frames[i] = chunk
+                                break
+        print(f"process is going to hard: {(list(dict.fromkeys(list_of_process_in_the_hard)))}")
+        # ==============================================================================================================
+        # remove process if part of it is not in list of frames
+        for i in range(len(ram.arr_of_frames)):
+            if ram.arr_of_frames[i] is not None:
+                for process in list_of_process:
+                    if ram.arr_of_frames[i] in process.splitProcess:
+                        is_all_in = True
+                        for chunk in process.splitProcess:
+                            if chunk in ram.arr_of_frames:
+                                continue
+                            else:
+                                is_all_in = False
+                                break
+                        if is_all_in is False:
+                            for i in range(len(ram.arr_of_frames)):
+                                if ram.arr_of_frames[i] in process.splitProcess:
+                                    ram.arr_of_frames[i] = None
+        # ==============================================================================================================
+
+        print(ram)
+
+
+ram = Ram(20, 2)
+process1 = Process("process1", 1, 5, 6, 16)
+process2 = Process("process2", 2, 4, 5, 4)
+process3 = Process("process3", 4, 8, 10, 8)
 list_of_process = [process1, process2, process3]
+
+set_frame_size(list_of_process, ram)
+print(process1.splitProcess)
+print(process2.splitProcess)
+print(process3.splitProcess)
 max_time = calculate_whole_time(list_of_process)
-print(max_time)
-print(earliest_deadline_first(list_of_process, max_time))
+print(f"max time is {max_time}")
+result_scheduling = earliest_deadline_first(list_of_process, max_time)
+calculate_memory_process(list_of_process, result_scheduling, max_time, ram)
